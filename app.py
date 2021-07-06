e90ba3e9c55445c3c3921ec07c71f980762c9508
Brian Liu <16676830+branlulu@users.noreply.github.com>
2021-07-06 17:16:44 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import json
+import torch
+from model import get_model
+from torchvision.transforms import transforms
+
+
+
+st.title('Pokemon Classifier')
+st.write("This is an app to classify the first 150 Pokemons.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+
+  # Define transformations for the image, should (note that imagenet models are trained with image size 224)
+    transformation = transforms.Compose([
+        transforms.CenterCrop(256),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # normalize to mean 0, std 1
+    ])
+    
+    # Preprocess the image
+    image_tensor = transformation(image).float()
+
+    # Add an extra batch dimension since pytorch treats all images as batches
+    image_tensor = image_tensor.unsqueeze_(0)
+
+    if torch.cuda.is_available():
+        image_tensor.cuda()
+
+    with open('pokemon_to_index.json') as data_file:    
+        class_indices = json.load(data_file)
+    index_to_class = list(class_indices.keys())
+
+    model = get_model()
+    probabilities = model(image_tensor).detach().numpy()[0]
+        
+    index = np.argsort(-probabilities)
+    
+    predictions = [index_to_class[i] for i in index]
+
+    st.write("RESULTS...")
+    for i in range(3):
+        st.write(predictions[i], ", score:", round(probabilities[index[i]], 3))
---
+++ model.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.nn.modules import activation
+from torch.nn.modules.pooling import MaxPool1d, MaxPool2d
+
+class PrintSize(nn.Module):
+  def __init__(self):
+    super(PrintSize, self).__init__()
+    
+  def forward(self, x):
+    print(x.shape)
+    return x
+
+class Net(nn.Module):
+    def __init__(self):
+      super(Net, self).__init__()
+
+      # First 2D convolutional layer, taking in 3 input channel (image),
+      # outputting 64 convolutional features, with a square kernel size of 3
+      self.conv1 = nn.Conv2d(3, 64, 3, stride=1, padding='same')
+      # Second 2D convolutional layer, taking in the 127 input layers,
+      # outputting 64 convolutional features, with a square kernel size of 3
+      self.conv2 = nn.Conv2d(64, 128, 3, stride=1, padding='same')
+
+      # Designed to ensure that adjacent pixels are either all 0s or all active
+      # with an input probability
+      self.dropout1 = nn.Dropout2d(0.25)
+      self.dropout2 = nn.Dropout2d(0.5)
+
+      # First fully connected layer
+      self.fc1 = nn.Linear(9216, 128)
+      # Second fully connected layer that outputs our 10 labels
+      self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        # Pass data though conv1
+        x =  self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        print(x.shape)
+        
+def get_model():
+    model = nn.Sequential(
+            PrintSize(),
+            nn.Conv2d(3, 64, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            PrintSize(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout(p=0.3),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(128, 256, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 256, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout(p=0.3),
+            nn.Flatten(),
+            nn.Linear(256*8*8, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 150),
+            nn.Softmax(dim=1)
+        )
+
+    return model
+
+
+random_data = torch.rand((1, 3, 256, 256))
+
+my_nn = Net()
+# print(my_nn)
+
+# def get_model():
+    # input_size_tup = (256, 256, 3)
+    # model = Sequential()
+    # model.add(Conv2D(filters=64,kernel_size=(3,3),padding='same',input_shape=input_size_tup,activation='relu'))
+    # model.add(MaxPool2D(pool_size=(2,2)))
+    # model.add(Conv2D(filters=128,kernel_size=(3,3),padding='same',input_shape=input_size_tup,activation='relu'))
+    # model.add(MaxPool2D(pool_size=(2,2)))
+    # model.add(Dropout(rate=0.3))
+    # model.add(BatchNormalization())
+    # model.add(Conv2D(filters=128,kernel_size=(3,3),padding='same',input_shape=input_size_tup,activation='relu'))
+    # model.add(MaxPool2D(pool_size=(2,2)))
+    # model.add(Conv2D(filters=256,kernel_size=(3,3),padding='same',input_shape=input_size_tup,activation='relu'))
+    # model.add(MaxPool2D(pool_size=(2,2)))
+    # model.add(BatchNormalization())
+    # model.add(Conv2D(filters=256,kernel_size=(3,3),padding='same',input_shape=input_size_tup,activation='relu'))
+    # model.add(MaxPool2D(pool_size=(2,2)))
+    # model.add(Dropout(rate=0.3))
+    # model.add(Flatten())
+    # model.add(Dense(1024,activation='relu'))
+    # model.add(Dropout(rate=0.3))
+    # model.add(Dense(512,activation='relu'))
+    # model.add(Dense(150,activation='softmax'))
+    # model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001))
+
+    # model.load_weights('CNN_model_weights.h5')
+    # return model
+    # return 0
